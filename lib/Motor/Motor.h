2907af499076ea5a6aba8eda6b2f3ce936e327d8
Gregory Tan <gtanhx04@gmail.com>
2021-12-07 11:50:08 +0800

    setup pio

---
+++ lib/Motor/Motor.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <Pins.h>
+
+class Motor{
+  public: 
+    Motor(int pwm, int a, int b){
+      
+      pinMode()
+    }
+}
---
+++ lib/Pins/Pins.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+
+#define FL_PWM 21
+#define FL_A 7
+#define FL_B 8
+#define FR_PWM 23
+#define FR_A 11
+#define FR_B 12
+#define BL_PWM 22
+#define BL_A 10
+#define BL_B 9
+#define BR_PWM 20
+#define BR_A 5
+#define BR_B 6
