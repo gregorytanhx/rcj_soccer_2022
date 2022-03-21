e0e160466d528de931cd8756ede900e391aa9117
Gregory Tan <gtanhx04@gmail.com>
2022-03-21 18:57:42 +0800

    fixed windows venv issue

--- DebugGUI/main.py
+++ DebugGUI/main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import math
 import sys
@@ -13,13 +12,13 @@ from tkinter import *
 
 
 bt_working = False
-try: 
+try:
     bluetooth = serial.Serial(port="COM10", baudrate=115200, timeout=0)
     bt_working = True
-except: 
+except:
     print("BLUETOOTH NOT ENABLED")
 # field dimensions
-OUTER_WIDTH = 182 
+OUTER_WIDTH = 182
 OUTER_HEIGHT = 243
 INNER_WIDTH = 132
 INNER_HEIGHT = 193
@@ -27,15 +26,15 @@ SCALE = 4
 BUFFER_SIZE = 12
 
 mode = {
-    "NormalProgram" : 1, 
-    "TestLocalisation" : 2, 
-    "TuneBallTrack" : 3, 
-    "RemoteControl" : 4
-} 
+    "NormalProgram": 1,
+    "TestLocalisation": 2,
+    "TuneBallTrack": 3,
+    "RemoteControl": 4,
+}
 
-#TODO: add user interaction for moving robot to point on field
-#TODO: add GUI for tuning ball track (can copy from simulator)
-#TODO: add GUI for switching debug modes
+# TODO: add user interaction for moving robot to point on field
+# TODO: add GUI for tuning ball track (can copy from simulator)
+# TODO: add GUI for switching debug modes
 
 # root = tk.Tk()
 # root.title("DEBUG")
@@ -86,36 +85,39 @@ mode = {
 
 # modeMenu.grid(row=9, column=1)
 
+
 def chooseMode():
     variable = StringVar(master)
-    variable.set("one") # default value
+    variable.set("one")  # default value
 
     w = OptionMenu(master, variable, "one", "two", "three")
     w.pack()
 
-def save():       
+
+def save():
     vals = [k.get(), a.get(), b.get(), c.get()]
-    
+
     with open("settings.pkl", "wb+") as f:
-        pickle.dump(vals, f) 
-        
- 
+        pickle.dump(vals, f)
+
 
 def shiftCoords(x, y):
-     # convert from centre origin to top left origin
+    # convert from centre origin to top left origin
     x += OUTER_WIDTH // 2
     y = OUTER_HEIGHT // 2 - y
     return x, y
+
 
 def getCoords(angle, distance):
     # convert mm dist to cm
     distance /= 10
     # convert coordinates from polar to cartesian
-    x = distance * math.sin(math.radians(angle)) 
-    y = distance * math.cos(math.radians(angle)) 
-    #print(f"Coordinates X: {int(x)} Y: {int(y)}")
-   
+    x = distance * math.sin(math.radians(angle))
+    y = distance * math.cos(math.radians(angle))
+    # print(f"Coordinates X: {int(x)} Y: {int(y)}")
+
     return x, y
+
 
 class SerialRecData:
     def __init__(self):
@@ -124,12 +126,12 @@ class SerialRecData:
         self.botDistance = 0
         # ball data
         self.ballAngle = 0
-        self.ballDist = 0       
+        self.ballDist = 0
         # bbox
         self.width = 0
         self.height = 0
 
-    
+
 class Ball(object):
     def __init__(self, x, y):
         self.x = x
@@ -137,14 +139,15 @@ class Ball(object):
         self.radius = 4 * SCALE
         self.img = img = pygame.image.load("ball.png")
         self.img = pygame.transform.scale(img, (self.radius, self.radius))
-        
-    def setPosition (self, coords):
+
+    def setPosition(self, coords):
         self.x = int(coords[0]) * SCALE
         self.y = int(coords[1]) * SCALE
 
     def draw(self, screen):
-        screen.blit(self.img, (self.x-self.radius // 2, self.y - self.radius // 2))
-        
+        screen.blit(self.img, (self.x - self.radius // 2, self.y - self.radius // 2))
+
+
 class Bot(object):
     def __init__(self, x, y):
         self.x = x
@@ -152,43 +155,45 @@ class Bot(object):
         self.radius = 9 * SCALE
         self.w = 0
         self.h = 0
-        
+
     def setBox(self, width, height):
         self.w = (width / 10) * SCALE
         self.h = (height / 10) * SCALE
+
     def setPosition(self, coords):
         self.x = int(coords[0]) * SCALE
         self.y = int(coords[1]) * SCALE
-            
+
     def draw(self, screen):
-        
         # draw bot
         pygame.draw.circle(screen, (40, 40, 40), (self.x, self.y), self.radius)
         # draw bounding box
         bbox = pygame.Rect(self.x - self.w / 2, self.y - self.h / 2, self.w, self.h)
         pygame.draw.rect(screen, (40, 40, 40), bbox, 4, border_radius=10)
-        
-        
+
+
 def getItem(buffer, i):
-    return int.from_bytes(buffer[2 * i : 2 * (i + 1)], byteorder = 'little')  
+    return int.from_bytes(buffer[2 * i : 2 * (i + 1)], byteorder="little")
+
 
 def getSerialData(data):
     while bluetooth.in_waiting >= BUFFER_SIZE + 1:
-        buffer = bytearray(bluetooth.readline())    
-        
-        data.botAngle = getItem(buffer, 0)  
-        data.botDistance = getItem(buffer, 1) 
-        data.ballAngle = getItem(buffer, 2) 
-        data.ballDist = getItem(buffer, 3) 
-        data.width = getItem(buffer, 4) 
-        data.height = getItem(buffer, 5) 
-    
+        buffer = bytearray(bluetooth.readline())
+
+        data.botAngle = getItem(buffer, 0)
+        data.botDistance = getItem(buffer, 1)
+        data.ballAngle = getItem(buffer, 2)
+        data.ballDist = getItem(buffer, 3)
+        data.width = getItem(buffer, 4)
+        data.height = getItem(buffer, 5)
+
+
 def sendSerialData(data):
     OFFSET_K = k.get()
     BALL_MULT_A = a.get()
     BALL_MULT_B = b.get()
     MAX_DIST = c.get()
-    
+
     vals = [OFFSET_K, BALL_MULT_A, BALL_MULT_B, MAX_DIST]
     sendData = []
     for i in range(len(vals)):
@@ -196,54 +201,54 @@ def sendSerialData(data):
         tmp = "f" if type(vals[i]) == float else "i"
         sendData += list(struct.pack(f"<{tmp}", vals[i]))
     bluetooth.write(bytearray(sendData))
-    
-       
-def main():   
+
+
+def main():
     pygame.init()
     screen = pygame.display.set_mode((OUTER_WIDTH * SCALE, OUTER_HEIGHT * SCALE))
-    
+
     ball = Ball(OUTER_WIDTH // 2, OUTER_HEIGHT // 2)
     bot = Bot(OUTER_WIDTH // 2, OUTER_HEIGHT // 2)
     run = True
     data = SerialRecData()
-   
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 bluetooth.close()
-                pygame.quit()     
-                   
+                pygame.quit()
+
         # getSerialData(data)
         # dummy test data
         data.botAngle = 30
         data.botDistance = 400
         data.width = 1000
         data.height = 300
-        
+
         # display position of robot and ball on field
 
         botX, botY = getCoords(data.botAngle, data.botDistance)
         relBallX, relBallY = getCoords(data.ballAngle, data.ballDist)
         ballX = relBallX + botX
         ballY = relBallY + botY
-        
+
         bot.setPosition(shiftCoords(botX, botY))
         bot.setBox(data.width, data.height)
         ball.setPosition(shiftCoords(ballX, ballY))
-        
+
         print(f"X confidence: {180 / data.width }")
         print(f"Y confidence: {180 / data.height }")
-        
-        draw_bg(screen)
-        ball.draw(screen)   
-        bot.draw(screen)
-        
-        pygame.display.update()
-        #root.update()
-    
 
-try: 
+        draw_bg(screen)
+        ball.draw(screen)
+        bot.draw(screen)
+
+        pygame.display.update()
+        # root.update()
+
+
+try:
     main()
 except:
     if bt_working:
